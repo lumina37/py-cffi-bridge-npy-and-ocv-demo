45d9a0fa576488904dfe022276d54fa96a0e8357
Starry-OvO <starry.qvq@gmail.com>
2023-09-20 20:04:50 +0800

    refactor: remove all hacks in pywrap.h

--- libfoo/algorithm/include/libfoo/pywrap.h
+++ libfoo/algorithm/include/libfoo/pywrap.h
@@ -1,20 +1,7 @@
 #pragma once
 
-#define PY_SSIZE_T_CLEAN // use Py_ssize_t instead of int
-
-#ifdef FOO_PYTHON_DEBUG
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#else
-#ifdef _DEBUG
-#undef _DEBUG // use these steps to avoid linking with python_d.lib
-#define __FOO_RESTORE_DEBUG
-#endif
-#include <Python.h>
-#ifdef __FOO_RESTORE_DEBUG
-#define _DEBUG
-#undef __FOO_RESTORE_DEBUG
-#endif
-#endif
 
 #define PY_ARRAY_UNIQUE_SYMBOL opencv_ARRAY_API
 #include <numpy/ndarrayobject.h>
